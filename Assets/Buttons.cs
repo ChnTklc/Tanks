0b30d03171ffa1d657397d4c842672fcaa57535a
ChnTklc <cihantoklucu1@gmail.com>
2016-03-21 18:58:16 +0200

    adadsf

---
+++ Assets/Buttons.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+public class Buttons : MonoBehaviour {
+
+    public void onStartButton()
+    {
+        SceneManager.LoadScene("Main");
+    }
+
+    public void onQuitButton()
+    {
+        Application.Quit();
+    }
+
+    public void clickWinRound()
+    {
+        SceneManager.LoadScene("WinRound");
+    }
+
+    public void clickBack()
+    {
+        SceneManager.LoadScene("Interface");
+    }
+    
+    public void clickToggle()
+    {
+
+    }
+}
